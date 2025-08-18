bf76296de8706a820c01d3a8b8109e93169d4aec
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2025-08-18 11:28:49 -0300

    Atividade Idade

--- IdadeAlunos/Program.cs
+++ IdadeAlunos/Program.cs
@@ -6,7 +6,26 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int idade1, idade2;
+
+            Console.WriteLine("Digite a Idade do primeiro aluno:");
+            idade1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Idade do segundo aluno:");
+            idade2 = int.Parse(Console.ReadLine());
+
+            if(idade1 > idade2)
+            {
+                Console.WriteLine("O primeiro aluno que tem: " + idade1 + " é mais velho que o segundo aluno que tem: " + idade2 + ".");
+            }
+            if (idade1 < idade2)
+            {
+                Console.WriteLine("O segundo aluno que tem: " + idade2 + " é mais velho que o primeiro aluno que tem: " + idade1 + ".");
+            }
+            if (idade1 == idade2)
+            {
+                Console.WriteLine("Os dois alunos têm a mesma idade que é: " + idade1 + ".");
+            }
         }
     }
 }
