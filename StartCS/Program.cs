71a0ffad7883b6606580b57a9432375d1b344bb5
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2025-08-18 11:22:22 -0300

    Aprendendo Operações

--- StartCS/Program.cs
+++ StartCS/Program.cs
@@ -6,7 +6,29 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n1, n2;
+            int r;
+
+            Console.WriteLine("Insira o valor 1:");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o valor 2:");
+            n2 = int.Parse(Console.ReadLine());
+
+            r = n1 + n2;
+
+            if (r == 0)
+            {
+                Console.WriteLine("A soma de " + n1 + " e " + n2 + " é zero.");
+            }
+            if (r > 0)
+            {
+                Console.WriteLine("A soma de " + n1 + " e " + n2 + " é " + r + " então é positiva.");
+            }
+            if (r < 0)
+            {
+                Console.WriteLine("A soma de " + n1 + " e " + n2 + " é " + r + " então é negativa.");
+            }
         }
     }
 }
