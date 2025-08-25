6108300687b01bb246ce3a259941f88c5a2420c5
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2025-08-25 10:54:19 -0300

    Metodos e Funções

---
+++ MetodosFuncoes/MetodosFuncoes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static double raio = 0;
+
+        static void Print(String value)
+        {
+            Console.WriteLine(value);
+        }
+        static void Print(double value)
+        {
+            Console.WriteLine(value);
+        }
+
+        static double AreaCirculo(double raio)
+        {
+            double r = 0;
+            r = Math.Pow(raio, 2) * Math.PI;
+
+            return r;
+        }
+
+        static void Main(string[] args)
+        {
+            Print("Digite o valor do raio: ");
+            raio = double.Parse(Console.ReadLine());
+            double area = AreaCirculo(raio);
+            Print("Area do circulo: " + area);
+            Print(area);
+        }
+    }
+}
