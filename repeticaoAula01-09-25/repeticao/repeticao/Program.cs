7c82ec5824dff710625caa1eef36b7078c993c74
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2025-09-01 11:35:14 -0300

    Aula dia 01/09/2025

---
+++ repeticaoAula01-09-25/repeticao/repeticao/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        const String mensagem = "Eu amo góticas!";
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void forFuction(String mensagem)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                // for -> parar, i de index
+                Console.WriteLine(mensagem);
+                Console.WriteLine($"index:{i}");
+            }
+        }
+
+        static void whileFunction(String mensagem, int index)
+        {
+            int i = 0;
+            while (i < index)
+            {
+                // while -> enquanto
+                Console.WriteLine(mensagem);
+                Console.WriteLine($"index:{i}");
+                i++;
+            }
+        }
+    }
+}
---
+++ trianguloPiramide/trinanguloPiramide/trinanguloPiramide/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variavel para controlar o numero de linhas, ou altura de triangulo
+            int linhas = 10;
+
+            // Variavel para controlar em qual linha estamos
+            int i = 0;
+
+            // Loop para desenhar o triangulo
+            while (i < linhas)
+            {
+                
+                // Variavel que controla os espaços em branco
+                int espacos = linhas - i - 1;
+
+                // Contador para os espaços em branco
+                int j = 0;
+
+                // Loop para desenhar os espaços em branco
+                while (j < espacos)
+                {
+                    Console.Write(" ");
+                    j++;
+                }
+
+                // Contador para os asteriscos
+                int k = 0;
+
+                // Loop para desenhar os asteriscos
+                while (k < 2 * i + 1)
+                {
+                    Console.Write("*");
+                    k++;
+                }
+
+                // Pula para a proxima linha
+                Console.WriteLine();
+
+                // Incrementa o contador de linhas
+                i++;
+            }
+        }
+    }
+}
