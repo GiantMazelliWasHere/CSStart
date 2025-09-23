b13083bb74cebef55a50e57bde46e5573578574b
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2025-09-22 22:30:36 -0300

    Terminando a tarefa carClass

--- CarClass/CarClass/Carro.cs
+++ CarClass/CarClass/Carro.cs
@@ -6,20 +6,45 @@ using System.Threading.Tasks;
 
 namespace CarClass
 {
+    /// <summary>
+    /// Representa a classe carro com os atributos chassis, numeroChassis, lincensePlate, cor e motor.
+    /// </summary>
     internal class Carro
     {
+        /// <summary>
+        /// Os atributos padrões de um carro.
+        /// </summary>
         private String chassis;
+        private String numeroChassis;
         private String licensePlate;
         private String cor;
         private String motor;
 
-        public Carro(String chassis, String licensePlate, String cor, String motor)
+        /// <summary>
+        /// Função construtora da classe carro.
+        /// </summary>
+        /// <param name="chassis">Tipo do chassis</param>
+        /// <param name="numeroChasis">Numero do chassis</param>
+        /// <param name="licensePlate">Placa do carro</param>
+        /// <param name="cor">Cor do carro</param>
+        /// <param name="motor">Motor do carro</param>
+        public Carro(String chassis, String numeroChasis, String licensePlate, String cor, String motor)
         {
-            this.chassis = chassis;
-            this.licensePlate = licensePlate;
-            this.cor = cor;
-            this.motor = motor;
+            this.Chassis = chassis;
+            this.NumeroChassis = numeroChasis;
+            this.LicensePlate = licensePlate;
+            this.Cor = cor;
+            this.Motor = motor;
             
         }
+
+        /// <summary>
+        /// Getters e Setters do atributo chassis, numeroChassis, licensePlate, cor e motor.
+        /// </summary>
+        public string Chassis { get => chassis; set => chassis = value; }
+        public string NumeroChassis { get => numeroChassis; set => numeroChassis = value; }
+        public string LicensePlate { get => licensePlate; set => licensePlate = value; }
+        public string Cor { get => cor; set => cor = value; }
+        public string Motor { get => motor; set => motor = value; }
     }
 }
--- CarClass/CarClass/Program.cs
+++ CarClass/CarClass/Program.cs
@@ -7,20 +7,28 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Carro sedan = new Carro("Sedan", "ABC-124", "Preto", "v8");
-            Carro suv = new Carro("SUV", "DEF-567", "Vermelho", "v12");
-            Carro esporte = new Carro("Esporte", "GKL-294", "Azul", "v6");
-            Carro familia = new Carro("Mini Van", "ABC-563", "Branco", "2.0 turbo");
-            Carro sedan2 = new Carro("Sedan", "GNL-106", "Cinza", "v6");
-            Carro conversivel = new Carro("Conversivel", "MLS-370", "Verde", "v12");
-            Carro suv2 = new Carro("SUV", "ABC-666", "Rosa", "v6");
-            Carro esporte2 = new Carro("Esporte", "GHL-806", "Laranja", "v8");
-            Carro conversivel2 = new Carro("Conversivel", "DFM-043", "Azul", "2.0 turbo");
-            Carro familia2 = new Carro("Mini Van", "ABC-906", "Preto", "v8");
+            /// Criando uma lista de carros e criando 10 novos objetos carros com seus atributos.
+            List<Carro> carList = new List<Carro>() {
+                new Carro("Sedan", "00001", "AKU-105", "Preto", "V6"),
+                new Carro("Hatch", "00002", "BLV-296", "Vermelho", "V8"),
+                new Carro("SUV", "00003", "CMW-317", "Azul", "W12"),
+                new Carro("Crossover", "00004", "DNX-428", "Branco", "V6"),
+                new Carro("Picape", "00005", "EOY-539", "Cinza", "V8"),
+                new Carro("Coupe", "00006", "FPZ-640", "Verde", "W12"),
+                new Carro("Conversivel", "00007", "GQA-862", "Rosa", "V6"),
+                new Carro("Perua", "00008", "HRB-862", "Laranja", "V8"),
+                new Carro("Minivan", "00009", "ISC-973", "Azul", "W12"),
+                new Carro("Roiadster", "00010", "JTD-084", "Preto", "V6"),
+            };
 
-            List<Carro> carros = new List<Carro>() { sedan, suv, esporte, familia, sedan2, conversivel, suv2, esporte2, conversivel2, familia2 };
+            /// Loop para imprimir os detalhes de cada carro na lista.
+            for (int i = 0; i < carList.Count; i++)
+            {
+                Console.WriteLine($"Detalhes do Carro {carList[i].Chassis}: ");
+                Console.WriteLine($"Numero do chassi: {carList[i].NumeroChassis}, Placa: {carList[i].LicensePlate} Cor: {carList[i].Cor}, Motor: {carList[i].Motor}");
+            }
 
-            
+
 
         }
     }
