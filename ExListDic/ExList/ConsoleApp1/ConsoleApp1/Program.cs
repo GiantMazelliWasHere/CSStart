21f88f36d6494ae3f9fd210b931527e1524da76d
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2026-02-23 11:21:30 -0300

    Exercicio Lista

---
+++ ExListDic/ExList/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> produtos = new List<string>();
+
+            produtos.Add("Notebook");
+            produtos.Add("Mouse");
+            produtos.Add("Teclado");
+            produtos.Add("Monitor");
+            produtos.Add("Mousepad");
+
+            foreach (string produto in produtos)
+            {
+                Console.WriteLine(produto);
+                
+            }
+            Console.WriteLine("-------------");
+
+            produtos.Remove("Monitor");
+
+            foreach (string produto in produtos)
+            {
+                Console.WriteLine(produto);
+            }
+        }
+    }
+}
